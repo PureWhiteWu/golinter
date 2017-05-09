ec975fce71cf2bcd96e58311be7fde3f8018b277
Pure White <daniel48@126.com>
2017-05-09 11:00:46 +0800

    fix bug

--- dispatch.go
+++ dispatch.go
@@ -15,7 +15,7 @@ func lintJava(tmpFile *os.File) (Result, error) {
 	lintOutput, _ := cmd.CombinedOutput()
 	re, _ := regexp.Compile(`(\[ERROR].*?)\n`)
 	tmp := re.FindAllStringSubmatch(string(lintOutput), -1)
-	log.Println(lintOutput)
+	log.Println(string(lintOutput))
 
 	// replace unnecessary information with ""
 	var lintErrors []string
